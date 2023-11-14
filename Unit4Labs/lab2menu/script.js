01c4c39d6e1c44dfb14db4440d921c65551f46d2
ajkruk <alicia.j.kruk@gmail.com>
2023-11-13 22:17:56 -0500

    lab 2 + 3

--- Unit4Labs/lab2menu/script.js
+++ Unit4Labs/lab2menu/script.js
@@ -1,3 +1,34 @@
-document.querySelector("#options").addEventListener("click", function(){
-    document.querySelector("#menu").style.display = 'flex'
+const options = document.querySelector("#options");
+const menu = document.querySelector("#menu");
+const lettersLink = document.querySelector("#lettersLink");
+const numbersLink = document.querySelector("#numbersLink");
+const letters = document.getElementById("#letters");
+const numbers = document.getElementById("#numbers");
+
+options.addEventListener("click", function() { 
+    if(menu.style.display ==='none') {
+        menu.style.display = 'flex'
+
+    }else {
+        menu.style.display = 'none'
+    }
+}) 
+
+lettersLink.addEventListener('click', function(e) {
+    e.preventDefault();
+    letters.style.display = 'block'
+    numbers.style.display = 'none'
+    menuBox.style.display = 'none'
 })
+
+numbersLink.addEventListener('click', function(e) {
+    e.preventDefault();
+    letters.style.display = 'none'
+    numbers.style.display = 'block'
+    menuBox.style.display = 'none'
+})
+
+
+// document.querySelector("#options").addEventListener("click", function(){
+//     document.querySelector("#menu").style.display = 'flex'
+// })
---
+++ Unit4Labs/lab3makeMoney/script.js
@@ -0,0 +1,52 @@
+
+const count = document.getElementById('count');
+const makeMoney = document.getElementById('makeMoney');
+const coinInput = document.getElementById('coinTypes');
+let totalMoney = document.getElementById('totalMoney');
+let total = 0.00;
+
+function makeCoin(coinType) {
+    let newCoin = document.createElement("div");
+    newCoin.classList.add(coinType.toLowerCase());
+    newCoin.innerText=coinType;
+    document.querySelector('main').append(newCoin);
+    switch(coinType) {
+        case 'Penny':
+            total += 0.01;
+            break;
+        case 'Nickel':
+            total += 0.05;
+            break;
+        case 'Dime':
+            total += 0.10;
+            break;
+        case 'Quarter':
+            total += 0.25;
+            break;
+    }
+    totalMoney.innerText = `Total Money: $${(Math.round(total * 100) / 100)}`
+    newCoin.addEventListener('click', function() {
+        document.querySelector('main').removeChild(newCoin);
+        switch(coinType) {
+            case 'Penny':
+                total -= 0.01;
+                break;
+            case 'Nickel':
+                total -= 0.05;
+                break;
+            case 'Dime':
+                total -= 0.10;
+                break;
+            case 'Quarter':
+                total -= 0.25;
+                break;
+        }
+        totalMoney.innerText = `Total Money: $${(Math.round(total * 100) / 100)}`
+    });
+}
+makeMoney.addEventListener('click', function(e) {
+    e.preventDefault();
+    for(let i=1; i<= count.value; i++) {
+        makeCoin(coinInput.value);
+    }
+});
